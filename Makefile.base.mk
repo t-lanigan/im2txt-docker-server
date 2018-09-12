dc5d6ddf55ce51f0cf68f4200028e4500be48695
Tyler Lanigan <tylerlanigan@gmail.com>
2018-09-11 17:28:25 -0700

    deleted some make targets and added to README.md

--- Makefile.base.mk
+++ Makefile.base.mk
@@ -1,16 +1,13 @@
 BUILD_ID := $(shell git rev-parse --short HEAD 2>/dev/null || echo no-commit-id)
 WORKSPACE := $(shell pwd)
-DEPLOYMENT_NAME := autotag
+DEPLOYMENT_NAME := autocaption
 
 # Names and locations for images/endpoints and S3 artifacts.
-S3_OUTPUT := s3://hs-machine-learning-processed-production/inbound-autotag/sm-output
-S3_INPUT := s3://hs-machine-learning-processed-production/inbound-autotag/data
 IMAGE_NAME := 112025830381.dkr.ecr.us-east-1.amazonaws.com/$(DEPLOYMENT_NAME)
 LATEST_IMAGE := $(IMAGE_NAME):latest
 ENDPOINT_NAME := $(DEPLOYMENT_NAME)-$(BUILD_ID)
 ENDPOINT_CONFIG_NAME := $(DEPLOYMENT_NAME)-$(BUILD_ID)-config
 AWS_REGION := us-east-1
-
 
 # Execution role to pass to SageMaker
 ROLE := arn:aws:iam::112025830381:role/sagemaker_deploy
@@ -47,61 +44,5 @@ build-service:
 	chmod +x container/$(DEPLOYMENT_NAME)/serve
 	bash container/build_and_push.sh $(DEPLOYMENT_NAME) $(BUILD_ID)
 	
-deploy-dev:
-	@echo "Not Implemented"
-
-deploy-staging:
-	@echo "Not Implemented"
-
-train:
-	aws sagemaker create-training-job --region $(AWS_REGION) \
-	--training-job-name $(DEPLOYMENT_NAME)-$(BUILD_ID) \
-	--hyper-parameters file://sagemaker/hyperparameters.json \
-	--algorithm-specification TrainingImage=$(IMAGE_NAME),\
-	TrainingInputMode="File" \
-	--role-arn $(ROLE) \
-	--input-data-config 'ChannelName=training,DataSource={S3DataSource={S3DataType=S3Prefix,S3Uri=$(S3_INPUT),S3DataDistributionType=FullyReplicated}},ContentType=string,CompressionType=None,RecordWrapperType=None' \
-	--output-data-config S3OutputPath=$(S3_OUTPUT) \
-	--resource-config file://sagemaker/train-resource-config.json \
-	--stopping-condition file://sagemaker/stopping-conditions.json
-	
-create-model:
-	aws sagemaker create-model --region $(AWS_REGION) \
-	--model-name $(DEPLOYMENT_NAME)-$(BUILD_ID) \
-	--primary-container Image=$(LATEST_IMAGE),\
-	ModelDataUrl=$(S3_OUTPUT)/$(DEPLOYMENT_NAME)-$(BUILD_ID)/output/model.tar.gz \
-	--execution-role-arn $(ROLE)
-
-create-endpoint-config:
-	aws sagemaker create-endpoint-config --region $(AWS_REGION) \
-	--endpoint-config-name $(ENDPOINT_CONFIG_NAME) \
-	--production-variants VariantName=$(DEPLOYMENT_NAME)-$(BUILD_ID),\
-	ModelName=$(DEPLOYMENT_NAME)-$(BUILD_ID),\
-	InitialInstanceCount=1,\
-	InstanceType="ml.c4.2xlarge",\
-	InitialVariantWeight=1.0
-
-create-endpoint:
-	aws sagemaker create-endpoint --region $(AWS_REGION) \
-	--endpoint-name $(DEPLOYMENT_NAME) \
-	--endpoint-config-name $(ENDPOINT_CONFIG_NAME)
-
-test-endpoint:
-	@aws sagemaker-runtime invoke-endpoint --body file://container/local_test/payload.json \
-	--endpoint-name $(DEPLOYMENT_NAME) \
-	--content-type application/json \
-	--accept application/json \
-	output.json
-	@cat output.json
-
-monitor-training-job:
-	python ./sagemaker/monitor_job.py training-job \
-	--job_name $(DEPLOYMENT_NAME)-$(BUILD_ID) \
-	--region $(AWS_REGION)
-
-monitor-endpoint-status:
-	python ./sagemaker/monitor_job.py endpoint \
-	--job_name $(DEPLOYMENT_NAME) \
-	--region $(AWS_REGION)
 
 
--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Im2txt Docker Server
+
+This project deploys a im2txt model in a docker container.
+
+In order to build a inference server into the container, we use the following stack:
+
+1. __[nginx][nginx]__ is a light-weight layer that handles the incoming HTTP requests and manages the I/O in and out of the container efficiently.
+2. __[gunicorn][gunicorn]__ is a WSGI pre-forking worker server that runs multiple copies of your application and load balances between them.
+3. __[flask][flask]__ is a simple web framework used in the inference app that you write. It lets you respond to call on the `/ping` and `/invocations` endpoints without having to write much code.
